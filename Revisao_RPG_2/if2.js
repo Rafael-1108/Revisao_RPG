67fd574b1577aa737fabf870a1e561c4bf57f3e8
Rafael-1108 <rafael.s.mendes.1108@gmail.com>
2025-08-04 09:37:29 -0300

    if

--- Revisao_RPG_2/if2.js
+++ Revisao_RPG_2/if2.js
@@ -12,10 +12,26 @@ if (vida >= 80) {
     console.log('Ayla está em estado crítico, recue!');
 }
 
-//parte 2 - missao
+//parte 2 - Missao
 
 if (vida >= 50 && energia >= 50) {
     console.log('Ayla entra no calabouço');
 } else {
     console.log('Ayla não pode seguir para o calabouço');
+}
+
+//parte 3 - Nivel
+
+if (xp >= 1000) {
+    console.log('Ayla subiu para o nível ', nivel, '.');
+} else {
+    console.log('Adquira mais xp para subir de nível');
+}
+
+//parte 4 - Ouro
+
+if (ouro >= 300) {
+    console.log('Ayla comprou a armadura mágica');
+} else {
+    console.log('Ayla não tem dinheiro sulficiente');
 }
