f67b43423ac7e60550faaf25a3f1f38e05a34879
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-08-04 13:40:14 -0300

    criacao.js

---
+++ Revisao_RPG_2/Refisao_RPG_1/criacao.js
@@ -0,0 +1,9 @@
+let nomePersonagem = 'Noah';
+let classe = 'guerreiro';
+let nivel = 99;
+let energia = 100;
+let moedas = 50;
+
+console.log(`Olá eu sou ${nomePersonagem}, minha classe é ${classe}, estou no nível ${nivel}, minha energia é 
+${energia} e tenho ${moedas} moedas!`);
+
