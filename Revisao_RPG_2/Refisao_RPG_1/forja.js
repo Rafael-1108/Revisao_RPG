984c207e40956e1cd3c36d16720a8f36db8ca932
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-08-04 13:40:40 -0300

    forja

---
+++ Revisao_RPG_2/Refisao_RPG_1/forja.js
@@ -0,0 +1,11 @@
+const NOMEARMA = 'Estilingue';
+const DANO = 55;
+const RARIDADE = 'Super mega Pro Max Lendária';
+const ELEMENTO = 'Ar';
+
+console.log('Dados do personagem:');
+console.log(`--------------------`);
+console.log(`Arma: ${NOMEARMA}`);
+console.log(`Dano da arma: ${NOMEARMA}`);
+console.log(`Raridade: ${RARIDADE}`);
+console.log(`Elemento: ${ELEMENTO}`);
