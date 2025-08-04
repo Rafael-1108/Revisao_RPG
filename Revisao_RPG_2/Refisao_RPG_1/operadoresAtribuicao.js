bb56b859ef85dbd733647f3ac4b2e8170a6c72aa
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-08-04 13:41:33 -0300

    operadores_atribuicao

---
+++ Revisao_RPG_2/Refisao_RPG_1/operadoresAtribuicao.js
@@ -0,0 +1,23 @@
+let nome = 'Ayla';
+let nivel = 1;
+let xp = 0;
+let ouro = 50;
+let vida = 100;
+
+console.log(`Início da Jornada de ${nome}`);
+console.log(`Nível: ${nivel}, XP: ${xp}, ouro: ${ouro}, Vida: ${vida}`);
+
+xp += 20;
+ouro += 100;
+vida -= 30;
+ouro *= 2;
+nivel++;
+vida/= 2
+
+console.log('Relatório final da jornada:');
+console.log('---------------------------');
+console.log('Herói: ' + nome);
+console.log('Nível: ' + nivel);
+console.log('XP: ' + xp);
+console.log('Ouro: ' + ouro);
+console.log('Vida: ' + vida);
